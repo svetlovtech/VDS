932bb81910264d6acf6b07c138d6f1608f75c014
Алексей Светлов <alexeysvetlov92@outlook.com>
2018-05-28 00:05:38 +0300

    Joining files

--- vdm.py
+++ vdm.py
@@ -43,13 +43,12 @@ class Area:
 
 class Vacancy:
     def __init__(self, id_number: int, area: Area, specialization: Specialization) -> None:
-        self._logger = logging.getLogger('__main__')
         self._id_number: int = id_number
         self._area: Area = area
         self._specialization: Specialization = specialization
         self._data: str = None
         self._url = f"https://api.hh.ru/vacancies/{self.id}"
-        self._logger.debug(str(self))
+        logging.debug(str(self))
 
     def __str__(self) -> str:
         return f'(Vacancy-' \
@@ -101,19 +100,34 @@ class Vacancy:
         self._specialization = value
 
 
-class VDSConfig:
-    logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
-                        level=logging.DEBUG)
+class InformationGuide:
+    @staticmethod
+    def get_response():
+        res = get('https://api.hh.ru/areas', headers=VDMConfig.headers)
+        with open(f"{str(datetime.datetime.utcnow()).replace(':', '').replace('-', '').replace('.', '')}"
+                  f"areas.txt", mode="w+", encoding="UTF8") as file:
+            file.write(res.text)
 
+    @staticmethod
+    def get_specializations():
+        res = get('https://api.hh.ru/specializations', headers=VDMConfig.headers)
+        with open(f"{str(datetime.datetime.utcnow()).replace(':', '').replace('-', '').replace('.', '')}"
+                  f"specializations.txt", mode="w+", encoding="UTF8") as file:
+            file.write(res.text)
+
+
+class VDMConfig:
     app_name = 'VDS'
     app_version = '0.1'
     app_email = 'alexeysvetlov92@gmail.com'
     headers = {'user-agent': f'{app_name}/{app_version} ({app_email})'}
 
+    logger_str_format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s'
+    logger_level = logging.DEBUG
+    logger_file_name = f"vdm{str(datetime.datetime.utcnow()).replace(':', '').replace('-', '').replace('.', '')}.log"
     area_ids: list = [
         Area('Moscow', 1),
     ]
-
     specializations_ids: list = [
         Specialization('Banking software', 1.395),
         Specialization('Testing', 1.117),
@@ -122,20 +136,13 @@ class VDSConfig:
 
 
 if __name__ == '__main__':
-    vacancy = Vacancy(25886815, VDSConfig.area_ids[0], VDSConfig.specializations_ids[0])
+    logging.basicConfig(format=VDMConfig.logger_str_format,
+                        level=VDMConfig.logger_level,
+                        filename=VDMConfig.logger_file_name)
+    vacancy = Vacancy(25886815, VDMConfig.area_ids[0], VDMConfig.specializations_ids[0])
     print(vacancy)
-    response = get(vacancy.url, headers=VDSConfig.headers)
+    response = get(vacancy.url, headers=VDMConfig.headers)
     vacancy.data = response.json()
     print(vacancy)
-
-    response = get('https://api.hh.ru/areas', headers=VDSConfig.headers)
-    with open(f"{str(datetime.datetime.utcnow()).replace(':', '').replace('-', '').replace('.', '')}"
-              f"areas.txt", mode="w+", encoding="UTF8") as file:
-        file.write(response.text)
-
-    response = get('https://api.hh.ru/specializations', headers=VDSConfig.headers)
-    with open(f"{str(datetime.datetime.utcnow()).replace(':', '').replace('-', '').replace('.', '')}"
-              f"specializations.txt", mode="w+", encoding="UTF8") as file:
-        file.write(response.text)
 
     """https://api.hh.ru/vacancies?area=1&specialization=1.117&page=0&per_page=1&no_magic=true"""
