408e787a07c401b671739c33b24b582a173d23db
Aleksey <alexeysvetlov92@gmail.com>
2018-05-30 01:21:53 +0300

    Added progressbar Simplified full script Refactoring Added multithreading

--- vds.py
+++ vds.py
@@ -1,130 +1,62 @@
-from requests import get
+from multiprocessing.dummy import Pool
 from argparse import ArgumentParser
+from requests import get
+from typing import List
+from tqdm import tqdm
 import logging
 import datetime
 
-
-class SetVacancyDataException(ValueError):
-    pass
-
-
-class Specialization:
-    def __init__(self, name: str, value: float) -> None:
-        self.__name = name
-        self.__number = value
-
-    def __str__(self) -> str:
-        return f'(Specialization-name:{self.name}; value:{self.number})'
-
-    @property
-    def name(self):
-        return self.__name
-
-    @property
-    def number(self):
-        return self.__number
+APP_NAME = 'VDS'
+__version__ = '0.0.1'
+APP_EMAIL = 'alexeysvetlov92@gmail.com'
+HEADERS = {'user-agent': f'{APP_NAME}/{__version__} ({APP_EMAIL})'}
+LOGGER_STR_FORMAT = u'%(filename)s[LINE:%(lineno)d] Level:%(levelname)-8s [%(asctime)s]  %(message)s'
+area_ids: list = [1]
+specializations_ids: list = [1.395, 1.117, 1.221]
+progressbar = None
 
 
-class Area:
-    def __init__(self, name: str, value: int) -> None:
-        self.__name = name
-        self.__number = value
-
-    def __str__(self) -> str:
-        return f'(Area-name:{self.name}; value:{self.number})'
-
-    @property
-    def name(self):
-        return self.__name
-
-    @property
-    def number(self):
-        return self.__number
-
-
-class Vacancy:
-    def __init__(self, id_number: int, area: Area, specialization: Specialization) -> None:
-        self._id_number: int = id_number
-        self._area: Area = area
-        self._specialization: Specialization = specialization
-        self._data: str = None
-        self._url = f"https://api.hh.ru/vacancies/{self.id}"
-        logging.debug(str(self))
-
-    def __str__(self) -> str:
-        return f'(Vacancy-' \
-               f'id_number:{self._id_number}; ' \
-               f'area:{self._area}; ' \
-               f'specialization:{self._specialization}; ' \
-               f'data:{self._data}; ' \
-               f'url:{self._url})'
-
-    @property
-    def url(self):
-        return self._url
-
-    @property
-    def id(self):
-        return self._id_number
-
-    @id.setter
-    def id(self, value):
-        self._id_number = value
-
-    @property
-    def data(self):
-        return self._data
-
-    @data.setter
-    def data(self, value: dict):
-        if type(value) is not dict:
-            raise SetVacancyDataException(f'Data must be a dictionary. Your data is {type(value)}')
-        if self._data is None:
-            self._data = value
+def get_vacancies_list(area_id: int, specialization_number: float) -> List[str]:
+    vacancies_list: List[str] = []
+    site = 'https://api.hh.ru/vacancies?'
+    page_number = 0
+    while True:
+        parameters = f"area={area_id}&" \
+                     f"specialization={specialization_number}&" \
+                     f"page={page_number}&" \
+                     f"per_page=100&" \
+                     f"no_magic=true&" \
+                     f"period=1"
+        response = get(site + parameters, headers=HEADERS).json()
+        for vacancies_data in response['items']:
+            vacancies_list.append(vacancies_data['url'])
+        if page_number == response['pages'] - 1:
+            break
         else:
-            raise SetVacancyDataException('Data is not none')
-
-    @property
-    def area(self):
-        return self._area
-
-    @area.setter
-    def area(self, value):
-        self._area = value
-
-    @property
-    def specialization(self):
-        return self._specialization
-
-    @specialization.setter
-    def specialization(self, value):
-        self._specialization = value
+            page_number += 1
+    return vacancies_list
 
 
-class Actions:
-    @staticmethod
-    def parsing_data():
-        vacancy = Vacancy(25886815, VDMConfig.area_ids[0], VDMConfig.specializations_ids[0])
-        response = get(vacancy.url, headers=VDMConfig.HEADERS)
-        vacancy.data = response.json()
-        logging.info(vacancy)
+def get_vacancy_data(url: str):
+    response = get(url, headers=HEADERS)
+    logging.info(response.text)
+    progressbar.update(1)
 
-        """https://api.hh.ru/vacancies?area=1&specialization=1.117&page=0&per_page=1&no_magic=true"""
-        pass
 
-    @staticmethod
-    def get_areas():
-        res = get('https://api.hh.ru/areas', headers=VDMConfig.HEADERS)
-        with open(f"areas{str(datetime.datetime.utcnow()).replace(':', '').replace('-', '').replace('.', '')}.txt",
-                  mode="w+", encoding="UTF8") as file:
-            file.write(res.text)
+def get_areas():
+    res = get('https://api.hh.ru/areas', headers=HEADERS)
+    with open(f"areas{str(datetime.datetime.utcnow()).replace(':', '').replace('-', '').replace('.', '')}.txt",
+              mode="w+", encoding="UTF8") as file:
+        file.write(res.text)
+        file.flush()
 
-    @staticmethod
-    def get_specializations():
-        res = get('https://api.hh.ru/specializations', headers=VDMConfig.HEADERS)
-        with open(f"specializations{str(datetime.datetime.utcnow()).replace(':', '').replace('-', '').replace('.', '')}"
-                  f".txt", mode="w+", encoding="UTF8") as file:
-            file.write(res.text)
+
+def get_specializations():
+    res = get('https://api.hh.ru/specializations', headers=HEADERS)
+    with open(f"specializations{str(datetime.datetime.utcnow()).replace(':', '').replace('-', '').replace('.', '')}"
+              f".txt", mode="w+", encoding="UTF8") as file:
+        file.write(res.text)
+        file.flush()
 
 
 def parse_args():
@@ -150,23 +82,6 @@ def parse_args():
     return parser.parse_args()
 
 
-class VDMConfig:
-    APP_NAME = 'VDS'
-    __version__ = '0.0.1'
-    APP_EMAIL = 'alexeysvetlov92@gmail.com'
-    HEADERS = {'user-agent': f'{APP_NAME}/{__version__} ({APP_EMAIL})'}
-
-    LOGGER_STR_FORMAT = u'%(filename)s[LINE:%(lineno)d] Level:%(levelname)-8s [%(asctime)s]  %(message)s'
-    area_ids: list = [
-        Area('Moscow', 1),
-    ]
-    specializations_ids: list = [
-        Specialization('Banking software', 1.395),
-        Specialization('Testing', 1.117),
-        Specialization('Development', 1.221),
-    ]
-
-
 if __name__ == '__main__':
     args = parse_args()
     logger_level = None
@@ -174,21 +89,40 @@ if __name__ == '__main__':
         logger_level = logging.DEBUG
     else:
         logger_level = logging.INFO
-    logging.basicConfig(format=VDMConfig.LOGGER_STR_FORMAT,
+    logging.basicConfig(format=LOGGER_STR_FORMAT,
                         level=logger_level,
                         filename=args.logfilename)
     logging.debug(args)
     logging.info('Starting vds...')
     if args.action is 'parsing':
         logging.info('Parsing data...')
-        Actions.parsing_data()
-        logging.info('Parsing data complete')
+        all_vacancies: List[str] = []
+        for area_number in area_ids:
+            for spec_number in specializations_ids:
+                for vacancy_url in get_vacancies_list(area_id=area_number, specialization_number=spec_number):
+                    all_vacancies.append(vacancy_url)
+        unique_vacancies = set()
+        for vacancy_url in all_vacancies:
+            unique_vacancies.add(vacancy_url)
+        logging.info(f'Statistic: all_vacancies {len(all_vacancies)}, unique_vacancies {len(unique_vacancies)}')
+        print(f'Statistic: all_vacancies {len(all_vacancies)}, unique_vacancies {len(unique_vacancies)}')
+        logging.debug('all_vacancies' + str(all_vacancies))
+        logging.debug('unique_vacancies' + str(unique_vacancies))
+        logging.info(f'Parsing data complete.')
+
+        progressbar = tqdm(total=len(unique_vacancies))
+        pool = Pool(args.threadpoolsize)
+        pool.map(get_vacancy_data, unique_vacancies)
+        pool.close()
+        pool.join()
+        progressbar.close()
+
     elif args.action is 'areas':
         logging.info('Getting areas data...')
-        Actions.get_areas()
+        get_areas()
         logging.info('Getting areas data complete')
     elif args.action is 'specializations':
         logging.info('Getting specializations data...')
-        Actions.get_specializations()
+        get_specializations()
         logging.info('Getting specializations data complete')
     logging.info('Stopping vds')
